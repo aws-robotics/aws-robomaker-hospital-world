73d156db8571b1c49399e49d8dd2b4a46f250850
Jeremy Wallace <jeremygw@amazon.com>
2020-10-05 16:57:38 -0400

    remove visitor chair, merge lighting fix, polish world files for publishing

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 pip install -r requirements.txt
 python3 fuel_utility.py download \
--m CGMClassic -m StorageRack -m VisitorChair=2 \
--m InstrumentCart1 -m OpScrubs -m PatientWheelChair \
+-m XRayMachine -m IVStand -m BloodPressureMonitor -m BPCart -m BMWCart \
+-m CGMClassic -m StorageRack -m Chair \
+-m InstrumentCart1 -m Scrubs -m PatientWheelChair \
 -m WhiteChipChair -m TrolleyBed -m SurgicalTrolley \
 -m PotatoChipChair -m VisitorKidSit -m FemaleVisitorSit \
 -m AdjTable -m MopCart3 -m MaleVisitorSit -m Drawer \
